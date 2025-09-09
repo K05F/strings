54b9434620d084d72a03a3ddf9d39d0d35a7df1f
K05F <firdaws.kanit90@gmail.com>
2025-09-09 12:37:32 +0200

    Update challenge9.c

--- strings/challenge9.c
+++ strings/challenge9.c
@@ -5,13 +5,14 @@ int main()
     char str[]= "azer rty bgh ";
     int a = 0;
   
-    for (int i = 0; i < str[i]; i++)
+    for (int i = 0; i < sizeof(str); i++)
     {
         if (str[i] != ' ' && str[i] != '\n')
         {
             str[a++] = str[i];
         }
     }
-    printf("%s", str[a]);
+    printf("%s", str);
     return 0;
+
 }
